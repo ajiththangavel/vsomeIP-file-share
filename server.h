59e02bedc00cfdd89198de5c5ce918ec55705d12
kanikattireddy <161320034+kanikattireddy@users.noreply.github.com>
2024-07-25 12:56:53 +0530

    Updated server.h

--- server.h
+++ server.h
@@ -1,32 +1,30 @@
-#ifndef SERVER_H
-#define SERVER_H
-#include <sstream>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
 #include <fstream>
+#include <cstring>
 #include <vsomeip/vsomeip.hpp>
-
+#include<thread>
 
 struct CustomPacket {
-        uint32_t sequence_number;
-        uint64_t timestamp;
-        uint32_t filename_length;
-        std::string filename;
-        uint32_t payload_length;
-        std::vector<vsomeip::byte_t> payload_data;
+    uint32_t sequence_number;
+    uint64_t timestamp;
+    std::string filename;
+    uint32_t payload_length;
+    std::vector<vsomeip::byte_t> payload_data;
 
-        // Serialize the CustomPacket structure into binary form
-        friend std::ostream& operator<<(std::ostream& os, const CustomPacket& packet) {
-            os.write(reinterpret_cast<const char*>(&packet.sequence_number), sizeof(packet.sequence_number));
-                    os.write(reinterpret_cast<const char*>(&packet.timestamp), sizeof(packet.timestamp));
-                    os.write(reinterpret_cast<const char*>(&packet.filename_length), sizeof(packet.filename_length));
-                    os.write(packet.filename.c_str(), packet.filename_length);
-                   // os.write(reinterpret_cast<const char*>(&packet.file_type_length), sizeof(packet.file_type_length));
-                   // os.write(packet.file_type.c_str(), packet.file_type_length);
-                    os.write(reinterpret_cast<const char*>(&packet.payload_length), sizeof(packet.payload_length));
-                    os.write(reinterpret_cast<const char*>(packet.payload_data.data()), packet.payload_data.size());
-            return os;
-        }
-    };
+    friend std::ostream& operator<<(std::ostream& os, const CustomPacket& packet) {
+        os.write(reinterpret_cast<const char*>(&packet.sequence_number), sizeof(packet.sequence_number));
+        os.write(reinterpret_cast<const char*>(&packet.timestamp), sizeof(packet.timestamp));
+        uint32_t filename_length = static_cast<uint32_t>(packet.filename.size());
+        os.write(reinterpret_cast<const char*>(&filename_length), sizeof(filename_length));
+        os.write(packet.filename.c_str(), filename_length);
+        os.write(reinterpret_cast<const char*>(&packet.payload_length), sizeof(packet.payload_length));
+        os.write(reinterpret_cast<const char*>(packet.payload_data.data()), packet.payload_data.size());
+        return os;
+    }
+};
 
 class Server
 {
@@ -38,7 +36,6 @@ public:
      void start();
 private:
      std::shared_ptr<vsomeip::application> app;
-
          // Constants declared as private members
          static const vsomeip::service_t SAMPLE_SERVICE_ID = 0x1234;
          static const vsomeip::instance_t SAMPLE_INSTANCE_ID = 0x5678;
@@ -46,6 +43,7 @@ private:
           void onMessage(const std::shared_ptr<vsomeip::message>& request);
 };
 
-#endif // SERVER_H
+
+
 
 
